10e314bf7e159766f67ca865319f1c5a4cef60c2
Amiya Dey <amiya1999dey@gmail.com>
2024-09-21 12:18:34 +0530

    updated basedURL in the frontend

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:8000/api'
+    baseURL: 'https://ai-image-generator-3ico.onrender.com/api'
 });
 
 export const getPosts = async () => await API.get('/post/');
